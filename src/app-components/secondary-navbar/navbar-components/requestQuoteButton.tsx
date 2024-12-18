adb104b638764c1744488500fd20794e867e990b
zlaam <zlaam.dev@gmail.com>
2024-12-18 14:16:53 +0500

    chore: Modify request Quote Button styles

--- src/app-components/secondary-navbar/navbar-components/requestQuoteButton.tsx
+++ src/app-components/secondary-navbar/navbar-components/requestQuoteButton.tsx
@@ -4,7 +4,7 @@ import React from "react";
 function RequestQuoteButton() {
   return (
     <Abutton
-      className="bg-background h-[60px]  border  font-semibold text-primary border-blue-200 rounded-full"
+      className="bg-background h-[60px]  border  font-semibold text-primary  rounded-full shadow-md active:scale-95 duration-200 transition-transform hover:border-dashed hover:border-blue-200"
       variant={"animate"}>
       Request Quote
     </Abutton>
