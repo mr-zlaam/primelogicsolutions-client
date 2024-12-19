4b8ffd537413e4c4779ff6d2859d11852f3d2242
zlaam <zlaam.dev@gmail.com>
2024-12-19 19:03:17 +0500

    feat: Add more fields to the Request Quote form

--- src/app-components/secondary-navbar/navbar-components/requestQuoteButton.tsx
+++ src/app-components/secondary-navbar/navbar-components/requestQuoteButton.tsx
@@ -15,25 +15,25 @@ export default function RequestQuoteButton() {
     }
   };
   return (
-    <div className="py-40 flex items-center justify-center ">
+    <div className="flex items-center justify-center ">
       <Modal>
-        <ModalTrigger className="bg-background h-[60px] border font-semibold text-primary rounded-full shadow-md  duration-200 transition-background hover:border-dashed hover:border-blue-200 hover:bg-primary hover:text-background ">
+        <ModalTrigger className="bg-background h-[60px] border font-semibold text-primary rounded-full shadow-md  duration-200 transition-background hover:border-dashed hover:border-blue-200 hover:bg-primary hover:text-background">
           Request Quote
         </ModalTrigger>
         <ModalBody>
-          <ModalContent className="flex flex-col md:flex-row w-full  p-4 md:p-8  rounded-lg">
-            <div className="w-full md:w-1/2  lg:flex items-center justify-center relative top-5 hidden">
+          <ModalContent className="flex flex-col md:flex-row w-full  p-4 md:p-8  rounded-lg  overflow-y-auto">
+            <div className="w-full md:w-1/2  lg:flex items-center justify-center  hidden relative top-5">
               <Image
                 src="https://i.postimg.cc/J0LhySrM/request-quote-modal.jpg"
                 alt="Placeholder Image"
                 width={600}
                 height={750}
-                className="rounded-lg object-cover"
+                className="rounded-lg object-cover w-full h-full"
               />
             </div>
-            <div className="w-full md:w-1/2 p-4 md:p-8 ">
+            <div className="w-full md:w-1/2 p-4 md:p-8">
               <h4 className="text-lg md:text-2xl text-neutral-600 font-bold mb-4">Request a Quote</h4>
-              <form className="space-y-4  w-full">
+              <form className="space-y-2  w-full">
                 <div className="grid w-full  items-center gap-1.5">
                   <Label htmlFor="name">Name</Label>
                   <Input
@@ -48,7 +48,7 @@ export default function RequestQuoteButton() {
                   <Input
                     type="email"
                     id="email"
-                    placeholder="Email"
+                    placeholder="Email your email"
                     className="w-full"
                   />
                 </div>
@@ -63,6 +63,24 @@ export default function RequestQuoteButton() {
                   />
                 </div>
 
+                <div className="grid w-full  items-center gap-1.5">
+                  <Label htmlFor="company">Company</Label>
+                  <Input
+                    type="text"
+                    id="company"
+                    placeholder="Enter your company name (Optional)"
+                    className="w-full"
+                  />
+                </div>
+                <div className="grid w-full  items-center gap-1.5">
+                  <Label htmlFor="deadline">Deadline</Label>
+                  <Input
+                    type="datetime-local"
+                    id="deadline"
+                    placeholder="Enter deadline (Optional)"
+                    className="w-full"
+                  />
+                </div>
                 <div className="grid w-full  items-center gap-1.5">
                   <Label htmlFor="address">Address</Label>
                   <Input
