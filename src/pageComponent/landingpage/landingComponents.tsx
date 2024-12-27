6bd0f53f67cbabcef33eb761ae51c7f288dcfacf
zlaam <zlaam.dev@gmail.com>
2024-12-27 16:13:15 +0500

    feat: Add hero6 Component Case Study

--- src/pageComponent/landingpage/landingComponents.tsx
+++ src/pageComponent/landingpage/landingComponents.tsx
@@ -4,6 +4,7 @@ import Hero2Component from "@/pageComponent/landingpage/hero2";
 import Hero3Component from "@/pageComponent/landingpage/hero3";
 import Hero4Component from "@/pageComponent/landingpage/hero4";
 import Hero5Component from "@/pageComponent/landingpage/hero5";
+import Hero6Component from "@/pageComponent/landingpage/hero6";
 function LandingComponents() {
   return (
     <React.Fragment>
@@ -12,6 +13,7 @@ function LandingComponents() {
       <Hero3Component />
       <Hero4Component />
       <Hero5Component />
+      <Hero6Component />
     </React.Fragment>
   );
 }
---
+++ src/types/index.d.ts
@@ -0,0 +1,7 @@
+export interface IPORTFOLIO {
+  title: string;
+  description: string;
+  image: string;
+  singleProjectHref: string;
+  singleProjectLivePreview: string;
+}
