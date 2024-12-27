ff9549c7f0cf90d649fb5557cf63af60ba603692
zlaam <zlaam.dev@gmail.com>
2024-12-27 19:13:59 +0500

    chore: Add hero7section in landing page

--- src/pageComponent/landingpage/hero7/index.tsx
+++ src/pageComponent/landingpage/hero7/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import UpperSection from "./upperSection/upperSection";
+import LowerSection from "./lowerSection/lowerSection";
 
 export default function Hero7Compnent() {
   return (
     <React.Fragment>
-      <section className="bg-[#06122A]">
+      <section className="bg-[#06122A] lg:max-h-[60vh] overflow-visible">
         <UpperSection />
+        <LowerSection />
       </section>
     </React.Fragment>
   );
--- src/pageComponent/landingpage/hero7/upperSection/upperSection.tsx
+++ src/pageComponent/landingpage/hero7/upperSection/upperSection.tsx
@@ -4,7 +4,7 @@ import UpperSectionContent from "./upperSectionContent/upperSectionContent";
 export default function UpperSection() {
   return (
     <React.Fragment>
-      <section className="lg:max-h-40vh">
+      <section className="lg:max-h-[40vh] mb-20">
         <UpperSectionContent />
       </section>
     </React.Fragment>
