81a6ab602cbe83d8d6e1e18cd4a207b8b153d29d
zlaam <zlaam.dev@gmail.com>
2024-12-29 22:48:06 +0500

    feat: Add another version of navbar

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,6 +1,7 @@
 import PrimaryNavBar from "@/appComponents/primaryNavbar";
-import SecondaryNavbar from "@/appComponents/secondary-navbar";
-import { menuData } from "@/appComponents/secondary-navbar/secondaryMenus";
+// import SecondaryNavbar from "@/appComponents/secondary-navbar";
+// import { menuData } from "@/appComponents/secondary-navbar/secondaryMenus";
+import { NavigationMenuDemo } from "@/appComponents/secondary-navbar/test-menu";
 import Footer from "@/pageComponent/footer/footer";
 import {} from "react";
 
@@ -8,9 +9,8 @@ function PageLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <PrimaryNavBar />
-
-      <SecondaryNavbar items={menuData} />
-
+      {/* <SecondaryNavbar items={menuData} /> */}
+      <NavigationMenuDemo />
       {children}
       <Footer />
     </>
